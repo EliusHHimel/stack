72113b0fcb5812f26de1ddfc31557d94d4885814
Hm ELius Hossain Himel <himelahmed201617@gmail.com>
2021-12-29 22:51:09 +0600

    stack example

---
+++ stack-example.js
@@ -0,0 +1,24 @@
+
+// Declaration of the stack
+const coins = [];
+console.log(coins);
+
+// Adding elements to the stack
+coins.push('Coin 1');
+console.log(coins);
+
+coins.push('Coin 2');
+console.log(coins);
+
+coins.push('Coin 3');
+console.log(coins);
+
+// Removing elements from the stack
+coins.pop();
+console.log(coins);
+
+coins.pop();
+console.log(coins);
+
+coins.pop();
+console.log(coins);
